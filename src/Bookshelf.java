ca86ba0a4a8bce381c761e7163a3b2ad56db81fa
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-08-07 22:42:33 +1000

    Function relocation
    relocated functions related to Library system commands to Library.java.
    
    Fixed exit command hopefully

--- src/Bookshelf.java
+++ src/Bookshelf.java
@@ -2,60 +2,10 @@ import java.util.Scanner;
 
 public class Bookshelf {
 
-    // public void getBooks() {
-    //     for (ArrayList<String> book : bookList) {
-    //         System.out.println(book.get(0) + "," + book.get(1));
-    //     }
-    // }
-
     Scanner sc = new Scanner(System.in);
 
-    public void commandList() {
-        String[] commands = {
-            "1 - getBooks",
-            "2 - addBook"
-        };
+    public void allBooks() {
 
-        System.out.println("Supported Commands:");
-        for (String command : commands) {
-            System.out.println(command);
-        }
-        commandSelector();
     }
 
-    public void commandSelector() {
-        Book bookCommands = new Book();
-        System.out.print("Select a command (1, 2...): ");
-        int command = sc.nextInt();
-
-        switch (command) {
-            case 1:
-                // bookCommands.getBooks();
-                break;
-            case 2:
-                bookCommands.createBook();
-                break;
-            default:
-                System.out.println("Invalid Command.");
-                commandSelector();
-
-        }
-    }
-
-    public void run() {
-    System.out.println("Welcome to the Library System!");
-    commandList();
-
-    while (true) {
-        System.out.println("Type 'exit' to quit, press enter to continue.");
-        String input = sc.nextLine();
-        if (input.equalsIgnoreCase("exit")) {
-            System.out.println("Exiting system...");
-            break;
-        }
-        else {
-            commandSelector();
-        }
-    }
-}
 }
--- src/Library.java
+++ src/Library.java
@@ -1,7 +1,60 @@
-public class Library {
-    public static void main(String[] args) {
-        Bookshelf BookSystem = new Bookshelf();
+import java.util.Scanner;
 
-        BookSystem.run();
+public class Library {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void commandList() {
+        String[] commands = {
+            "1 - getBooks",
+            "2 - addBook"
+        };
+
+        System.out.println("Supported Commands:");
+        for (String command : commands) {
+            System.out.println(command);
+        }
+        commandSelector();
+    }
+
+    public void commandSelector() {
+        Book bookCommands = new Book();
+        System.out.print("Select a command (1, 2...): ");
+        int command = sc.nextInt();
+
+        switch (command) {
+            case 1:
+                // bookCommands.getBooks();
+                break;
+            case 2:
+                bookCommands.createBook();
+                break;
+            default:
+                System.out.println("Invalid Command.");
+                commandSelector();
+
+        }
+    }
+
+    public void run() {
+        System.out.println("Welcome to the Library System!");
+
+        while (true) {
+            commandList();
+            commandSelector();
+
+            System.out.println("Type 'exit' to quit, press enter to continue.");
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting system...");
+                break;
+            }
+        }
+
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        new Library().run();
     }
 }
